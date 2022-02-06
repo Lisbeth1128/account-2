4e3bdcc096c41c0876d95d92d294990009f54b15
Lisbeth <13511707@qq.com>
2022-02-07 00:10:56 +0800

    搞定导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,67 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  border: 1px solid re;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  display: flex;
+  > ul {
+    width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+  }
+`
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route index element={<Money />} />
-        <Route path="/tags" element={<Tags />} />
-        <Route path="/statistics" element={<Statistics />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>页面不存在，你丫输错地址了吧！</p>
-            </main>
-          }
-        />
-      </Routes>
+      <Wrapper>
+        <Main>
+          <Routes>
+            <Route index element={<Money />} />
+            <Route path="/tags" element={<Tags />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>页面不存在，你丫输错地址了吧！</p>
+                </main>
+              }
+            />
+          </Routes>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/">记账页</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
     </div>
   );
 }
@@ -31,11 +74,6 @@ function Money() {
       <main>
         <h2>记账页</h2>
       </main>
-      <nav>
-        <Link to="/">记账页</Link>
-        <Link to="/tags">标签页</Link>
-        <Link to="/statistics">统计页</Link>
-      </nav>
     </>
   );
 }
@@ -46,11 +84,6 @@ function Tags() {
       <main>
         <h2>标签页</h2>
       </main>
-      <nav>
-        <Link to="/">记账页</Link>
-        <Link to="/tags">标签页</Link>
-        <Link to="/statistics">统计页</Link>
-      </nav>
     </>
   );
 }
@@ -61,11 +94,6 @@ function Statistics() {
       <main>
         <h2>统计页</h2>
       </main>
-      <nav>
-        <Link to="/">记账页</Link>
-        <Link to="/tags">标签页</Link>
-        <Link to="/statistics">统计页</Link>
-      </nav>
     </>
   );
 }
