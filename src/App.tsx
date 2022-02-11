a1eee5b2008b6f444d622125d7f877c09785c6c5
Lisbeth <13511707@qq.com>
2022-02-12 01:48:00 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,36 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import styled from "styled-components";
-import Nav from './components/Nav'
+import Layout from './components/Layout'
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`
 
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`
 
 
 function App() {
   return (
     <div className="App">
-      <Wrapper>
-        <Main>
-          <Routes>
-            <Route index element={<Money />} />
-            <Route path="/tags" element={<Tags />} />
-            <Route path="/statistics" element={<Statistics />} />
-            <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>页面不存在，你丫输错地址了吧！</p>
-                </main>
-              }
-            />
-          </Routes>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Routes>
+        <Route index element={<Money />} />
+        <Route path="/tags" element={<Tags />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>页面不存在，你丫输错地址了吧！</p>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
 }
 function Money() {
   return (
     <>
-      <main>
+      <Layout>
         <h2>记账页</h2>
-      </main>
+      </Layout>
     </>
   );
 }
@@ -55,9 +40,9 @@ function Money() {
 function Tags() {
   return (
     <>
-      <main>
+      <Layout>
         <h2>标签页</h2>
-      </main>
+      </Layout>
     </>
   );
 }
@@ -65,9 +50,9 @@ function Tags() {
 function Statistics() {
   return (
     <>
-      <main>
+      <Layout>
         <h2>统计页</h2>
-      </main>
+      </Layout>
     </>
   );
 }
