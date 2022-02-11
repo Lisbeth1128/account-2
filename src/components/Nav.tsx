bff5668724c593114353d2871e7e3d16d61dbc2a
Lisbeth <13511707@qq.com>
2022-02-12 02:30:06 +0800

    修复 Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,14 +12,16 @@ const NavWrapper = styled.nav`
     > li {
         width: 33.3333%;
         text-align: center;
-        padding: 4px 0;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        .icon {
-            width: 24px;
-            height: 24px;
+        > a {
+            padding: 4px 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            .icon {
+              width: 24px;
+              height: 24px;
+            }
         }
     }
   }
@@ -30,16 +32,13 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name="money" />
-                    <Link to="/">记账页</Link>
+                    <Link to="/"><Icon name="money" />记账页</Link>
                 </li>
                 <li>
-                    <Icon name="tag" />
-                    <Link to="/tags">标签页</Link>
+                    <Link to="/tags"><Icon name="tag" />标签页</Link>
                 </li>
                 <li>
-                    <Icon name="chart" /> 
-                    <Link to="/statistics">统计页</Link>
+                    <Link to="/statistics"><Icon name="chart" />统计页</Link>
                 </li>
             </ul>
         </NavWrapper>
