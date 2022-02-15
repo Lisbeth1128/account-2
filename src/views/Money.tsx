7af5afd806bf86de048a977e1d96e701261de366
Lisbeth <13511707@qq.com>
2022-02-16 01:35:16 +0800

    Money.tsx 之 数字面板 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -55,7 +55,7 @@ const CategorySection = styled.section`
     > li {
       width: 50%;
       text-align: center;
-      padding: 16px 0;
+      padding: 18px 0;
       position: relative;
       &.selected::after {
         content: '';
@@ -71,33 +71,91 @@ const CategorySection = styled.section`
   }
 `
 
-const NumberPadSection = styled.section``
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background-color: #F0F0F0;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+                inset 0 5px 5px -5px rgba(0, 0, 0, 0.25)
+  }
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+    &.ok {
+      height: 128px;
+      float: right;
+    }
+    &.zero {
+      width: 50%;
+    }
+    &:nth-child(1) {
+      background-color: #F2F2F2;
+    }
+    &:nth-child(2),
+    &:nth-child(5) {
+      background-color: #E0E0E0;
+    }
+    &:nth-child(3),
+    &:nth-child(6),
+    &:nth-child(9) {
+      background-color: #D3D3D3;
+    }
+    &:nth-child(4),
+    &:nth-child(7),
+    &:nth-child(10) {
+      background-color: #C1C1C1;
+    }
+    &:nth-child(8),
+    &:nth-child(11),
+    &:nth-child(13) {
+      background-color: #B8B8B8;
+    }
+    &:nth-child(12) {
+      background-color: #9A9A9A;
+    }
+    &:nth-child(14) {
+      background-color: #A9A9A9;
+    }
+  }
+}
+`
 function Money() {
-    return (
-      <>
-        <Layout>
-          <TagsSection>
-            <ol>
-              <li>衣</li>
-              <li>食</li>
-              <li>住</li>
-              <li>行</li>
-            </ol>
-            <button>新增标签</button>
-          </TagsSection>
-          <NotesSection>
-            <label>
-              <span>备注</span>
-              <input type="text" placeholder='在这里添加备注'/>
-            </label>
-          </NotesSection>
-          <CategorySection>
-            <ul>
-              <li className="selected">支出</li>
-              <li>收入</li>
-            </ul>
-          </CategorySection>
-          <NumberPadSection>
+  return (
+    <>
+      <Layout>
+        <TagsSection>
+          <ol>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+          </ol>
+          <button>新增标签</button>
+        </TagsSection>
+        <NotesSection>
+          <label>
+            <span>备注</span>
+            <input type="text" placeholder='在这里添加备注' />
+          </label>
+        </NotesSection>
+        <CategorySection>
+          <ul>
+            <li className="selected">支出</li>
+            <li>收入</li>
+          </ul>
+        </CategorySection>
+        <NumberPadSection>
+          <div className="output">100</div>
+          <div className="pad clearfix">
             <button>1</button>
             <button>2</button>
             <button>3</button>
@@ -109,13 +167,14 @@ function Money() {
             <button>7</button>
             <button>8</button>
             <button>9</button>
-            <button>OK</button>
-            <button>0</button>
-            <button>.</button>
-          </NumberPadSection>
-        </Layout>
-      </>
-    );
+            <button className="ok">OK</button>
+            <button className="zero">0</button>
+            <button className="dot">.</button>
+          </div>
+        </NumberPadSection>
+      </Layout>
+    </>
+  );
 }
 
 export default Money
