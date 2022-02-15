dfe1fcf08cf99558a603a3f7503b083ffc33d6c6
Lisbeth <13511707@qq.com>
2022-02-16 01:55:28 +0800

    Money.tsx UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components'
 const TagsSection = styled.section`
   background-color: #FFFFFF;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   > ol {
     margin: 0 -12px;
     > li {
@@ -128,10 +133,17 @@ const NumberPadSection = styled.section`
   }
 }
 `
+
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`
+
+
 function Money() {
   return (
     <>
-      <Layout>
+      <MyLayout>
         <TagsSection>
           <ol>
             <li>衣</li>
@@ -172,7 +184,7 @@ function Money() {
             <button className="dot">.</button>
           </div>
         </NumberPadSection>
-      </Layout>
+      </MyLayout>
     </>
   );
 }
