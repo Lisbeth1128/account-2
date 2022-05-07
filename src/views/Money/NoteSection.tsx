fc7d334853c66b00d8802b022a574a9ed7922929
Lisbeth <13511707@qq.com>
2022-05-08 02:19:51 +0800

    创建单独的 Input

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,25 +1,11 @@
-import { useRef } from "react";
+import { Input } from "components/Input";
+import { ChangeEventHandler, useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.section`
   background-color: #f5f5f5;
   padding: 0px 16px;
   font-size: 14px;
-  > label {
-    display: flex;
-    align-items: center;
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `
 type Props = {
   value: string,
@@ -30,22 +16,14 @@ const NoteSection: React.FC<Props> = (props) => {
   //const [note, setNote] = useState('')
   const note = props.value
   const refInput = useRef<HTMLInputElement>(null)
-  const onBlur = () => {
-    if(refInput.current !== null){
-      //setNote(refInput.current.value)
-      props.onChange(refInput.current.value)
-    }
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+      props.onChange(e.target.value)
   }
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text" placeholder="在这里添加备注"
-               ref={refInput}
-               defaultValue={note}
-               onBlur={onBlur}
-        />
-      </label>
+      <Input label="备注" type="text" value={note} onChange={onChange}
+              placeholder="请填写备注"
+      />
     </Wrapper>
   )
 }
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -3,11 +3,21 @@ import Icon from 'components/Icon'
 import Layout from 'components/Layout'
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 import {useTags} from 'useTags'
 
 type Params = {
   id: string
 }
+
+const Topbar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background-color: white;
+`
 
 const Tag: React.FC = () => {
     const { findTag } = useTags()
@@ -15,10 +25,11 @@ const Tag: React.FC = () => {
     const tag = findTag(parseInt(id!))
     return (
       <Layout>
-        <header>
+        <Topbar>
           <Icon name="left" />
           <span>编辑标签</span>
-        </header>
+          <Icon />
+        </Topbar>
         <div>
           <label>
             <span>标签名</span>
