971e62f29963163959b06afab523f3614ef8e1df
Lisbeth <13511707@qq.com>
2022-05-10 01:29:42 +0800

    tags 数据持久化

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -25,7 +25,7 @@ const TagList = styled.ol`
 `
 
 function Tags() {
-    const {tags} = useTags()
+    const {tags, addTag} = useTags()
     return (
       <>
         <Layout>
@@ -43,7 +43,7 @@ function Tags() {
             <Space />
             <Space />
             <Space />
-            <Button>新增标签</Button>
+            <Button onClick={addTag}>新增标签</Button>
           </Center>
         </Layout>
       </>
