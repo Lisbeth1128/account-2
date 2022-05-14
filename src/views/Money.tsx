62bd1428c6fa0ba0c723e975ad38805db1453e2f
Lisbeth <13511707@qq.com>
2022-05-15 03:01:37 +0800

    展示所有记录

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,15 +1,18 @@
 import Layout from '../components/Layout'
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import {TagsSection} from './Money/TagsSection'
-import {NoteSection} from './Money/NoteSection'
-import {CategorySection} from './Money/CategorySection'
-import {NumberPadSection} from './Money/NumberPadSection'
+import { TagsSection } from './Money/TagsSection'
+import { NoteSection } from './Money/NoteSection'
+import { CategorySection } from './Money/CategorySection'
+import { NumberPadSection } from './Money/NumberPadSection'
 import { useRecords } from 'hooks/useRecords'
 
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
+`
+const CategoryWrapper = styled.div`
+  background-color: #c4c4c4;
 `
 
 type Category = '-' | '+'
@@ -23,7 +26,7 @@ const defaultFormData = {
 
 function Money() {
   const [selected, setSelected] = useState(defaultFormData)
-  const {addRecord} = useRecords()
+  const { addRecord } = useRecords()
 
   const onChange = (obj: Partial<typeof selected>) => {
     setSelected({
@@ -33,7 +36,7 @@ function Money() {
   }
 
   const submit = () => {
-    if(addRecord(selected)){
+    if (addRecord(selected)) {
       alert('保存成功')
       setSelected(defaultFormData)
     }
@@ -43,14 +46,16 @@ function Money() {
     <>
       <MyLayout>
         <TagsSection value={selected.tagIds}
-                     onChange={tagIds => onChange({tagIds})}/> 
+          onChange={tagIds => onChange({ tagIds })} />
         <NoteSection value={selected.note}
-                     onChange={note => onChange({note})}/>
-        <CategorySection value={selected.category}
-                         onChange={category => onChange({category})}/>
+          onChange={note => onChange({ note })} />
+        <CategoryWrapper>
+          <CategorySection value={selected.category}
+            onChange={category => onChange({ category })} />
+        </CategoryWrapper>
         <NumberPadSection value={selected.amount}
-                          onChange={amount => onChange({amount})}
-                          onOk={submit}/>
+          onChange={amount => onChange({ amount })}
+          onOk={submit} />
       </MyLayout>
     </>
   );
